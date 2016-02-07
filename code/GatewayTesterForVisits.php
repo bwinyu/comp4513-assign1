edb86d7ff33bdacfd21b5de6806311d37240588a
Matthew Grixti <matthewpcgrixti@gmail.com>
2016-02-06 17:08:33 -0700

    Create VisitsTableGateway and Visits classes and modified test.
    Create the following classes:
    Visits
    VisitsTableGateway - Added filterByCountryCode()
    
    Modified GatewayTesterForVisits to test the two new classes above.

--- code/GatewayTesterForVisits.php
+++ code/GatewayTesterForVisits.php
@@ -119,6 +119,21 @@ echo '<h3>Test findById(3)</h3>';
 $result = $gate->findById(3);
 echo $result->id . " - " . $result->name;
 
+//Test for Visits
+echo '<hr/>';
+echo '<h2>Test VisitsTableGateway</h2>';
+
+echo '<h3>Test findAllSorted()</h3>';
+$gate = new VisitsTableGateway($dbAdapter);
+$result = $gate->filterByCountryCode('BF');
+foreach ($result as $row) {
+    echo $row->id . " - " . $row->ip_address . "<br/>";
+}
+
+echo '<h3>Test findById(3)</h3>';
+$result = $gate->findById(3);
+echo $result->id . " - " . $result->ip_address;
+
 
 
 // all done close connection
