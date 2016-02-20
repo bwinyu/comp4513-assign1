373c6258ea4abbaebae9ff1bfd11740f24f77e6a
Matthew <matthewpcgrixti@gmail.com>
2016-02-19 22:36:15 -0700

    Added visitsByCountry() method.

--- code/GatewayTesterForVisits.php
+++ code/GatewayTesterForVisits.php
@@ -12,116 +12,116 @@ require_once('lib/helpers/visits-setup.inc.php');
 
 <?php
 
-echo '<hr/>';
-echo '<h2>Test BrowserTableGateway</h2>';
-echo '<h3>Test findAll()</h3>';
-$gate = new BrowserTableGateway($dbAdapter);
-$result = $gate->findAll();
-foreach ($result as $row)
-{
-   echo $row->id . " - " . $row->name . "<br/>";
-}
-
-echo '<h3>Test findById(3)</h3>';
-$result = $gate->findById(3);
-echo $result->id . " - " . $result->name;
-  
-
-
-echo '<hr/>';
-echo '<h2>Test DeviceBrandTableGateway</h2>';
-echo '<h3>Test findAllSorted()</h3>';
-$gate = new DeviceBrandTableGateway($dbAdapter);
-$result = $gate->findAllSorted(true);
-foreach ($result as $row)
-{
-   echo $row->id . " - " . $row->name . "<br/>";
-}
-
-echo '<h3>Test findById(11)</h3>';
-$result = $gate->findById(11);
-echo $result->id . " - " . $result->name;
-
-//Test for Continents
-echo '<hr/>';
-echo '<h2>Test ContinentsTableGateway</h2>';
-
-echo '<h3>Test findAllSorted()</h3>';
-$gate = new ContinentsTableGateway($dbAdapter);
-$result = $gate->findAllSorted(true);
-foreach ($result as $row) {
-    echo $row->ContinentCode . " - " . $row->ContinentName . "<br/>";
-}
-
-echo '<h3>Test findById(OC)</h3>';
-$result = $gate->findById('OC');
-echo $result->ContinentCode . " - " . $result->ContinentName;
-
-
-//Test for Countries
-echo '<hr/>';
-echo '<h2>Test CountriesTableGateway</h2>';
-
-echo '<h3>Test findAllSorted()</h3>';
-$gate = new CountriesTableGateway($dbAdapter);
-$result = $gate->findAllSorted(true);
-foreach ($result as $row) {
-    echo $row->ISO . " - " . $row->CountryName . "<br/>";
-}
-
-echo '<h3>Test findById(BF)</h3>';
-$result = $gate->findById('BF');
-echo $result->ISO . " - " . $result->CountryName;
-
-//Test for DeviceTypes
-echo '<hr/>';
-echo '<h2>Test DeviceTypesTableGateway</h2>';
-
-echo '<h3>Test findAllSorted()</h3>';
-$gate = new DeviceTypesTableGateway($dbAdapter);
-$result = $gate->findAllSorted(true);
-foreach ($result as $row) {
-    echo $row->id . " - " . $row->name . "<br/>";
-}
-
-echo '<h3>Test findById(3)</h3>';
-$result = $gate->findById(3);
-echo $result->id . " - " . $result->name;
-
-//Test for OperatingSystems
-echo '<hr/>';
-echo '<h2>Test OperatingSystemsTableGateway</h2>';
-
-echo '<h3>Test findAllSorted()</h3>';
-$gate = new OperatingSystemsTableGateway($dbAdapter);
-$result = $gate->findAllSorted(true);
-foreach ($result as $row) {
-    echo $row->id . " - " . $row->name . "<br/>";
-}
-
-echo '<h3>Test findById(3)</h3>';
-$result = $gate->findById(3);
-echo $result->id . " - " . $result->name;
-
-
-//Test for Referrers
-echo '<hr/>';
-echo '<h2>Test ReferrersTableGateway</h2>';
-
-echo '<h3>Test findAllSorted()</h3>';
-$gate = new ReferrersTableGateway($dbAdapter);
-$result = $gate->findAllSorted(true);
-foreach ($result as $row) {
-    echo $row->id . " - " . $row->name . "<br/>";
-}
-
-echo '<h3>Test findById(3)</h3>';
-$result = $gate->findById(3);
-echo $result->id . " - " . $result->name;
-
-//Test for Visits
-echo '<hr/>';
-echo '<h2>Test VisitsTableGateway</h2>';
+//echo '<hr/>';
+//echo '<h2>Test BrowserTableGateway</h2>';
+//echo '<h3>Test findAll()</h3>';
+//$gate = new BrowserTableGateway($dbAdapter);
+//$result = $gate->findAll();
+//foreach ($result as $row)
+//{
+//   echo $row->id . " - " . $row->name . "<br/>";
+//}
+//
+//echo '<h3>Test findById(3)</h3>';
+//$result = $gate->findById(3);
+//echo $result->id . " - " . $result->name;
+//
+//
+//
+//echo '<hr/>';
+//echo '<h2>Test DeviceBrandTableGateway</h2>';
+//echo '<h3>Test findAllSorted()</h3>';
+//$gate = new DeviceBrandTableGateway($dbAdapter);
+//$result = $gate->findAllSorted(true);
+//foreach ($result as $row)
+//{
+//   echo $row->id . " - " . $row->name . "<br/>";
+//}
+//
+//echo '<h3>Test findById(11)</h3>';
+//$result = $gate->findById(11);
+//echo $result->id . " - " . $result->name;
+//
+////Test for Continents
+//echo '<hr/>';
+//echo '<h2>Test ContinentsTableGateway</h2>';
+//
+//echo '<h3>Test findAllSorted()</h3>';
+//$gate = new ContinentsTableGateway($dbAdapter);
+//$result = $gate->findAllSorted(true);
+//foreach ($result as $row) {
+//    echo $row->ContinentCode . " - " . $row->ContinentName . "<br/>";
+//}
+//
+//echo '<h3>Test findById(OC)</h3>';
+//$result = $gate->findById('OC');
+//echo $result->ContinentCode . " - " . $result->ContinentName;
+//
+//
+////Test for Countries
+//echo '<hr/>';
+//echo '<h2>Test CountriesTableGateway</h2>';
+//
+//echo '<h3>Test findAllSorted()</h3>';
+//$gate = new CountriesTableGateway($dbAdapter);
+//$result = $gate->findAllSorted(true);
+//foreach ($result as $row) {
+//    echo $row->ISO . " - " . $row->CountryName . "<br/>";
+//}
+//
+//echo '<h3>Test findById(BF)</h3>';
+//$result = $gate->findById('BF');
+//echo $result->ISO . " - " . $result->CountryName;
+//
+////Test for DeviceTypes
+//echo '<hr/>';
+//echo '<h2>Test DeviceTypesTableGateway</h2>';
+//
+//echo '<h3>Test findAllSorted()</h3>';
+//$gate = new DeviceTypesTableGateway($dbAdapter);
+//$result = $gate->findAllSorted(true);
+//foreach ($result as $row) {
+//    echo $row->id . " - " . $row->name . "<br/>";
+//}
+//
+//echo '<h3>Test findById(3)</h3>';
+//$result = $gate->findById(3);
+//echo $result->id . " - " . $result->name;
+//
+////Test for OperatingSystems
+//echo '<hr/>';
+//echo '<h2>Test OperatingSystemsTableGateway</h2>';
+//
+//echo '<h3>Test findAllSorted()</h3>';
+//$gate = new OperatingSystemsTableGateway($dbAdapter);
+//$result = $gate->findAllSorted(true);
+//foreach ($result as $row) {
+//    echo $row->id . " - " . $row->name . "<br/>";
+//}
+//
+//echo '<h3>Test findById(3)</h3>';
+//$result = $gate->findById(3);
+//echo $result->id . " - " . $result->name;
+//
+//
+////Test for Referrers
+//echo '<hr/>';
+//echo '<h2>Test ReferrersTableGateway</h2>';
+//
+//echo '<h3>Test findAllSorted()</h3>';
+//$gate = new ReferrersTableGateway($dbAdapter);
+//$result = $gate->findAllSorted(true);
+//foreach ($result as $row) {
+//    echo $row->id . " - " . $row->name . "<br/>";
+//}
+//
+//echo '<h3>Test findById(3)</h3>';
+//$result = $gate->findById(3);
+//echo $result->id . " - " . $result->name;
+//
+////Test for Visits
+//echo '<hr/>';
+//echo '<h2>Test VisitsTableGateway</h2>';
 
 //echo '<h3>Test filterByC0untryCode(BF)</h3>';
 //$gate = new VisitsTableGateway($dbAdapter);
@@ -152,8 +152,11 @@ echo '<h2>Test VisitsTableGateway</h2>';
 
 $gate = new VisitsTableGateway($dbAdapter);
 echo '<h3>Test countBy</h3>';
-$result = $gate->countByOS('1');
-echo $result;
+$gate = new countriesTableGateway($dbAdapter);
+$result = $gate->visitsByCountry();
+foreach ($result as $row) {
+    echo $row->CountryName . " - " . $row->Visits . "<br/>";
+}
 
 
 // all done close connection
--- code/lib/model/CountriesTableGateway.class.php
+++ code/lib/model/CountriesTableGateway.class.php
@@ -43,4 +43,20 @@ class CountriesTableGateway extends TableDataGateway
 
         return $results;
     }
+    /**
+     * Retrieves all countries and counts the number of IP visits from the visits table
+     *
+     * @return array - array of objects for each record returned
+     */
+    public function visitsByCountry()
+    {
+
+        $sql = "SELECT countries.CountryName, Count(visits.ID) AS Visits FROM countries INNER JOIN visits ON countries.ISO = visits.country_code GROUP BY countries.CountryName HAVING Count(visits.ID) > 10";
+
+        $results = $this->dbAdapter->fetchAsArray($sql);
+        if (is_null($results))
+            return $results;
+        else
+            return $this->convertRecordsToObjects($results);
+    }
 }?>
