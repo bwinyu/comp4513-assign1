92e26fd775ee7873e25c12cc9da92f78f4b22d6f
pyoun908 <pyoun908@mtroyal.ca>
2016-02-21 11:10:19 -0700

    added charts and charts-config.php
    Charts page and helper code

--- code/charts.php
+++ code/charts.php
@@ -1,12 +1,52 @@
 <?php 
 $headertext = "Charts";
 include "includes/head.inc.php";
+include "lib/helpers/charts-config.php";
 ?>
 
-<div class="page-content mdl-grid">
 
 <!-- content here -->
-   
-</div>
+
+<form action="charts.php" method="get">
+
+    <div class="page-content mdl-grid">
+        <div class="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col">
+            <div class="mdl-card__title mdl-color--teal-500">
+                <h2 class="mdl-card__title-text">Visits Per Month</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+
+
+                Select a month to chart visits:
+                <select name="month">
+                    <option value="Jan">January</option>
+                    <option value="Feb">February</option>
+                    <option value="Mar">March</option>
+                    <option value="Apr">April</option>
+                </select>
+
+            </div>
+
+        </div>
+
+        <div class="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col">
+            <div class="mdl-card__title mdl-color--teal-500">
+                <h2 class="mdl-card__title-text">Geo Chart</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+
+            </div>
+        </div>
+
+        <div class="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col">
+            <div class="mdl-card__title mdl-color--teal-500">
+                <h2 class="mdl-card__title-text">Visit Data Column Chart</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+
+            </div>
+        </div>
+    </div>
+</form>
 
 <?php include "includes/footer.inc.php" ?>
