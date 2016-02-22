3913c8744c8bd0d776d9956b6e2714571ced6a87
Philip Young <pyoun908@mtroyal.ca>
2016-02-21 17:44:01 -0700

    added charts code for chart 1 and 2 / jquery.js reference

--- code/includes/head.inc.php
+++ code/includes/head.inc.php
@@ -7,17 +7,18 @@
       <link rel="stylesheet" href="https://code.getmdl.io/1.1.1/material.css">
       <link rel="stylesheet" href="css/styles.css">
       <script defer src="https://code.getmdl.io/1.1.1/material.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
    </head>
-   
+
    <body>
       <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer">
-         
+
          <header class="mdl-layout__header mdl-color--grey-100">
             <div class="mdl-layout__header-row">
                <span class="mdl-layout-title mdl-color-text--black"><?php echo $headertext; ?></span>
             </div>
          </header>
-         
+
          <div class="mdl-layout__drawer mdl-color--blue-grey-900">
             <span class="mdl-layout-title mdl-color-text--white">Admin Dashboard</span>
             <nav class="mdl-navigation mdl-color--blue-grey-800">
---
+++ code/lib/helpers/charts.js
@@ -0,0 +1,59 @@
+
+<!--Loads JavaScript for Google Charts-->
+$(function() {
+    google.charts.load('current', {packages: ['corechart' , 'geochart']});
+
+
+    $("#areaChartBtn").click(function() {
+        google.charts.setOnLoadCallback(drawAreaChart);
+        drawAreaChart();
+    })
+
+    function drawAreaChart() {
+        //$.get( "code/api.php", function( data ) {
+        //    $( ".result" ).html( data );
+        //    alert( "Load was performed." );
+        //});
+        var data = google.visualization.arrayToDataTable([
+            ['Day', 'Visits'],
+            ['2016-01-01',  197],
+            ['2016-01-02',  211],
+            ['2016-01-03',  194],
+            ['2016-01-04',  213]
+        ]);
+        var currMonth = $("#areaChartMonth option:selected").text();
+        var options = {
+            title: currMonth + ' Visits',
+            hAxis: {title: 'Day',  titleTextStyle: {color: '#333'}},
+            vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('areaChart'));
+        chart.draw(data, options);
+    }
+
+    function drawGeoChart() {
+        google.charts.load('current', {'packages':['geochart']});
+        google.charts.setOnLoadCallback(drawRegionsMap);
+
+        var data = google.visualization.arrayToDataTable([
+            ['Country', 'Popularity'],
+            ['Germany', 200],
+            ['United States', 300],
+            ['Brazil', 400],
+            ['Canada', 500],
+            ['France', 600],
+            ['RU', 700]
+        ]);
+
+        var options = {};
+
+        var chart = new google.visualization.GeoChart(document.getElementById('geoChart'));
+
+        chart.draw(data, options);
+
+    }
+});
+
+
+
